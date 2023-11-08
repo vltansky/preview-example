f6e5455b23a2240879b30296bd60ad6c3d262df0
Vlad Tansky <vl.tansky@gmail.com>
2023-11-08 09:40:41 +0200

    x

--- app/page.tsx
+++ app/page.tsx
@@ -106,6 +106,9 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
+          <span>
+            <Link href="/test">GO</Link>
+          </span>
         </a>
       </div>
     </main>
